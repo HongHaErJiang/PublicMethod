d2433ec93f21f269f51e9e025bfdd8112fc92ded
zhugongyuan <1172807717@qq.com>
2017-05-12 17:44:11 +0800

    UITextView 实现占位符 最大高度 实时改变高度 字数检测

---
+++ EditTextView.h
@@ -0,0 +1,24 @@
+//
+//  EditTextView.h
+//  UItextView
+//
+//  Created by 朱公园 on 2017/5/12.
+//  Copyright © 2017年 zhugongyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#define TEXTCOUNT @"TEXTCOUNT"
+#define TEXTVIEWHEIGHT @"textViewHeight"
+
+@interface EditTextView : UITextView
+
+@property(nonatomic,strong)NSString * placeHolder; //占位文字
+@property(nonatomic,strong)UIColor * textEditColor; //设置文本颜色
+@property(nonatomic,strong)UIFont * fontEdit; //字体
+
+@property(nonatomic, assign)CGFloat maxHeight; //textView最大高度设置，如果有，就设置，否则高度随着文字高度变化而变化
+
+@property(nonatomic, assign)NSInteger textMaxCount; //设置最大输入文本个数
+
+
+@end
